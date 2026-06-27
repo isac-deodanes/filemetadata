bd87f0d7ad9747e89cafa032c55434547e9c2b15
isac-deoanes <isacdeodanes@gmail.com>
2026-06-27 17:52:21 -0600

    mostrando-informacion-del-archivo

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
 var express = require('express');
 var cors = require('cors');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
-const multer  = require('multer');
+
 
 var app = express();
 
 
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-
-
-app.use(cors({ origin: '*' }));
-
-// Middleware manual para asegurar que los encabezados de control de acceso se inyecten siempre
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-
-
-
+app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
-app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   if (!req.file) {
-    return res.json({ error: "seleccione un archivo valido" });
+    return res.status(400).json({
+      error: 'No file uploaded'
+    });
   }
 
-  res.json({
+  return res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size
@@ -46,10 +27,13 @@ app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
 });
 
 
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+ 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
-
 
